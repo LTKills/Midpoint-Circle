e79401b9d8882d9484ccef701e60027d60a76715
Gabriel <ltkills@github.com>
2018-03-24 15:03:45 -0300

    i'm blue da rum bi da rum ba

--- testPixels.py
+++ testPixels.py
@@ -15,11 +15,11 @@ canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state='normal')
 
 
 for x in range(WIDTH):
-    img.put("#ffffff", (x, int(HEIGHT/2)))
-    img.put("#ffffff", (x, 100))
+    img.put("#6666ff", (x, int(HEIGHT/2)))
+    img.put("#6666ff", (x, 100))
 
 for y in range(HEIGHT):
-    img.put("#ffffff", (int(WIDTH/2), y))
-    img.put("#ffffff", (100, y))
+    img.put("#6666ff", (int(WIDTH/2), y))
+    img.put("#6666ff", (100, y))
 
 window.mainloop()
